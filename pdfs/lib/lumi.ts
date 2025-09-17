7ce124e11df6da4f281721902628012c684cd361
KoudShe <alexandroidfree@gmail.com>
2025-09-17 16:40:54 -0300

    lumi.ts
    new file

---
+++ pdfs/lib/lumi.ts
@@ -0,0 +1,10 @@
+
+import { createClient } from '@lumi.new/sdk'
+
+export const lumi = createClient({
+  projectId: 'p357699888304050176',
+  apiBaseUrl: 'https://api.lumi.new',
+  authOrigin: 'https://auth.lumi.new',
+  baseUrl: 'https://sexual-performance-ebook.lumi.ing',
+})
+
